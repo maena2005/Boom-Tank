d1c53ea642eb4b4ac8bee5bd9bb66237bfc1a466
Maéna <maenabouet@gmail.com>
2024-04-10 01:01:28 +0200

    tout

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public string Desert;
+    public string Settings;
+    
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(Desert);
+    }
+
+    public void SettingButton()
+    {
+        SceneManager.LoadScene(Settings);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
